ad6858050b7717c076bb60cb9918c0c8259fa3fd
D3fc0IV <frederic.delval@gmail.com>
2016-09-23 09:38:34 +0200

    index file divided

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fredericd
+ * Date: 23-09-16
+ * Time: 09:17
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <title>HeadFile</title>
+    <style type="text/css">
+        body {
+            padding-top: 50px;
+            background-color: #d35400;
+            color: #FFF;
+        }
+        .starter-template {
+            padding: 40px 15px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
